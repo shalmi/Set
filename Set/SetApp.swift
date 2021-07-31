36e9fa11ba3745aab283fcd13cd13ed07ee140a6
Michael Yeroshalmi <shalmi913@gmail.com>
2021-07-31 14:48:20 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Michael on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
